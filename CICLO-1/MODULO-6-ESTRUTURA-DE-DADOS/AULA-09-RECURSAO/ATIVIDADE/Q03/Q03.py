555967e777aa1e38484ac35427c527179be7eb71
rubenfreita <rubensfreitas65@gmail.com>
2024-02-05 11:25:23 -0300

    criando questao 3 da atividade da aula 9 de estrutura de dados

---
+++ CICLO-1/MODULO-6-ESTRUTURA-DE-DADOS/AULA-09-RECURSAO/ATIVIDADE/Q03/Q03.py
@@ -0,0 +1,11 @@
+"""
+---
+**EXERCÍCIO 03 | SOMA DOS ELEMENTOS DE UMA LISTA**
+
+Criar uma função recursiva que some todos os elementos de uma lista. A função deve aceitar uma lista de números como entrada e retornar a soma de seus elementos.
+
+Instruções
+1. A lista de números deve ser passada como argumento para a função.
+2. A função deve percorrer a lista de maneira recursiva, somando cada elemento ao total.
+3. O resultado final deve ser a soma total dos elementos da lista.
+"""
